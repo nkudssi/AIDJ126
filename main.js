6ba303fd8fce80b147c0cc5ff718bdcff54a827c
nkudssi <90203207+nkudssi@users.noreply.github.com>
2023-04-05 14:42:11 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
+song = "";
+song$ = "";
 function preload(){
-
+song = loadSound("music.mp3")
+song$ = loadSound("old_phone.mp3")
 }
 
 function setup(){
 canvas = createCanvas(800,600);
 canvas.center();
-
+video = createCapture(VIDEO);
+video.hide();
 }
 
 function draw(){
-
+    image(video,0,0,800,600);
 }
